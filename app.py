49ae1fade35dad2290991475e8897f43baedc969
Oscarj-jqt <jacquetoscar0@gmail.com>
2025-01-09 14:43:49 +0100

    config package unidecode

--- app.py
+++ app.py
@@ -71,4 +71,5 @@ app.secret_key = os.getenv("SECRET_KEY", "fallback_default_key")
 
 
 if __name__ == "__main__":
+    port = int(os.environ.get("FLASK_PORT", 5000)) 
     app.run(debug=True, port=FLASK_PORT)
